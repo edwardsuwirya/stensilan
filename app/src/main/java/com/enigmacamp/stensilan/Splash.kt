97fe6b15b4b220d5a6facb15531aff9f2b28471b
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-07-17 07:28:04 +0700

    Add Splash Screen and Pink Theme https://coolors.co/355070-6d597a-b56576-e56b6f-eaac8b

---
+++ app/src/main/java/com/enigmacamp/stensilan/Splash.kt
@@ -0,0 +1,20 @@
+package com.enigmacamp.stensilan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
