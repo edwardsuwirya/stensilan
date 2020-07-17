ed9ec9c126f70c79b49baf976fd7d6348b477153
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-07-17 08:23:11 +0700

    Search Toolbar

--- app/src/main/java/com/enigmacamp/stensilan/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/stensilan/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.enigmacamp.stensilan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+
+private val TAG = MainActivity::class.java.simpleName
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        val searchStensil = menu.findItem(R.id.app_bar_search)
+        val searchView = searchStensil.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d(TAG, query)
+                if (query.isNullOrEmpty()) {
+                    return false
+                } else {
+                    return true
+                }
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+        return true
     }
 }
